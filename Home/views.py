7874c590fc6de3bfde3ac8ae213160f9759f299d
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-04-23 20:24:27 +0530

    Contact Database

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from Home.models import Contact
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        desc = request.POST.get('message')
+        contact = Contact(email=email, desc=desc, date=datetime.today())
+        contact.save()
+
     return render(request, 'home.html')
