2f477a50f9646151cee3c53021266987d69d4c2b
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-04-23 21:41:11 +0530

    Static Image Update

--- Home/urls.py
+++ Home/urls.py
@@ -3,9 +3,12 @@
 from django.contrib import admin
 from django.urls import path
 from Home import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path("", views.home, name='Home'),
     path("Home", views.home, name='home')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
